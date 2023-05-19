f7acb8f2999a26a1a0e94717bdc1f0f4f5f27e2d
Ihor Nikitenko <Nikitenkoivi@gmail.com>
2023-05-19 14:44:45 +0300

    Add request for char list

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -13,16 +13,18 @@ class MarvelService {
 
     getAllCharacters = async () => {
         const res = await this.getResource(`${this._apiBase}/characters?limit=9&offset=210&${this._apiKey}`);
-        return res.data.results.map(this._transformCharacter())
+        return res.data.results.map(item => this._transformCharacter(item))
     }
 
-    getAllCharacter = async (id) => {
+    getCharacter = async (id) => {
         const res = await this.getResource(`${this._apiBase}/characters/${id}?${this._apiKey}`);
         return this._transformCharacter(res.data.results[0]);
     }
 
     _transformCharacter = (char) => {
+
         return {
+            id: char.id,
             name: char.name,
             description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
